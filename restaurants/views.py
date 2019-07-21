62781644eb2bea04d4d3d44ff7a80f1aeb4b7555
Nawaf ALrifai <nbalrifai@gmail.com>
2019-07-21 16:45:16 +0300

    upload

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Restaurant
-from .forms import RestaurantForm, SignupForm, SigninForm
+from .models import Restaurant, Item
+from .forms import RestaurantForm, SignupForm, SigninForm, ItemForm
 from django.contrib.auth import login, authenticate, logout
 
 def signup(request):
@@ -50,29 +50,46 @@ def restaurant_list(request):
 
 
 def restaurant_detail(request, restaurant_id):
+    restaurant = Restaurant.objects.get(id=restaurant_id)    
     context = {
-        "restaurant": Restaurant.objects.get(id=restaurant_id)
+        "restaurant": restaurant,
+        "item" : Item.objects.filter(restaurant=restaurant)
     }
     return render(request, 'detail.html', context)
+
 
 def restaurant_create(request):
     form = RestaurantForm()
     if request.method == "POST":
         form = RestaurantForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            rest = form.save(commit=False)
+            rest.owner = request.user
+            rest.save()
             return redirect('restaurant-list')
     context = {
         "form":form,
     }
     return render(request, 'create.html', context)
 
-def item_create(request):
 
+def item_create(request, restaurant_id):
+    rest = Restaurant.objects.get(id=restaurant_id)
+
+    form = ItemForm()
+    if request.method == "POST":
+        form = ItemForm (request.POST)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.restaurant = rest 
+            item.save() 
+            return redirect ("restaurant-detail", restaurant_id)
     context = {
-        
+        "form" : form,
+        "rest": rest   
     }
     return render(request, 'item_create.html', context)
+
 
 def restaurant_update(request, restaurant_id):
     restaurant_obj = Restaurant.objects.get(id=restaurant_id)
